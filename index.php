988c29999461971cf633cb4ad0e5cfe3a2a04d46
m.rizki akbar <m.rizki.x31@gmail.com>
2025-10-29 11:14:41 +0700

    percobaan

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    nfil
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure veniam recusandae harum accusamus doloremque, quidem quam laboriosam dolor dolores non corrupti provident illum dicta voluptatum! Tenetur quisquam laudantium similique.
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia repellat tenetur eaque culpa, dolores id quibusdam minus ea ullam dicta, voluptatem, sapiente neque incidunt adipisci molestias porro! Repellat, ratione rem.
 </body>
 </html>

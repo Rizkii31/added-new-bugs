1602e8a29b0aa61b27d330885cd5b3cfcf7eef94
m.rizki akbar <m.rizki.x31@gmail.com>
2025-10-29 11:15:49 +0700

    percobaann

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore iure veniam recusandae harum accusamus doloremque, quidem quam laboriosam dolor dolores non corrupti provident illum dicta voluptatum! Tenetur quisquam laudantium similique.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia repellat tenetur eaque culpa, dolores id quibusdam minus ea ullam dicta, voluptatem, sapiente neque incidunt adipisci molestias porro! Repellat, ratione rem.
+   ini master
 </body>
 </html>
